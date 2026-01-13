a2a9850f5005dc5b51bee8764e422f9de80ef75b
edvansanchoo <edvansanchoo@gmail.com>
2026-01-12 21:04:40 -0400

    Implementado agente com funcionalidade de descrever o conteudo de uma imagem

--- Python_GPT_crie_seu_chatbot_com_IA/helpers.py
+++ Python_GPT_crie_seu_chatbot_com_IA/helpers.py
@@ -2,6 +2,7 @@ import base64
 import cv2
 import numpy as np
 import os
+import uuid
 
 def carrega(nome_do_arquivo):
     try:
@@ -20,3 +21,17 @@ def salva(nome_do_arquivo, conteudo):
             arquivo.write(conteudo)
     except IOError as e:
         print(f"Erro ao salvar arquivo: {e}")
+
+def encodar_imagem(caminho_imagem):
+    with open(caminho_imagem, "rb") as arquivo_imagem:
+        return base64.b64encode(arquivo_imagem.read()).decode('utf-8')
+    
+def salvar_imagem(caminho_salvar, imagem_enviada):
+    nome_arquivo = str(uuid.uuid4()) + os.path.splitext(imagem_enviada.filename)[1]
+    caminho_arquivo = os.path.join(caminho_salvar, nome_arquivo)
+    imagem_enviada.save(caminho_arquivo)
+    return caminho_arquivo
+
+def deletar_imagem(caminho_imagem):
+    if os.path.exists(caminho_imagem):
+        os.remove(caminho_imagem)
